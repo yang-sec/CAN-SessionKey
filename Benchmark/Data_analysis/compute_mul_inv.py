55205c3acb6ce3b21b55d914c51994d9b19b9359
shishishi123 <38490617+shishishi123@users.noreply.github.com>
2020-08-25 21:00:42 -0400

    Add files via upload

---
+++ Benchmark/Data_analysis/compute_mul_inv.py
@@ -0,0 +1,21 @@
+import numpy as np
+m = 257
+invs = '0'
+
+def str_append(s, n):
+    output = ''
+    i = 0
+    while i < n:
+        output += s
+        i = i + 1
+    return output
+
+for i in range(m):
+	if i==0:
+		continue
+	a = i % m
+	for x in range(1,m):
+		if (a*x)%m==1:
+			invs = invs + ',' + str(x)
+
+print(invs)
